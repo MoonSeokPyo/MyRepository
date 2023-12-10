f782dcab56f9308c7c2812d0f6d57ca9d1c9d732
SeokPyo Moon <happy1es@naver.com>
2023-12-11 00:50:47 +0900

    A에 main 메서드 추가

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -3,5 +3,7 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello git! I'm A");
+		
+		System.out.println("This is test Java program ver_1.0.");
 	}
 }
