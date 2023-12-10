2790477ad427d77a3dac7e6db00be0319cc386e9
SeokPyo Moon <happy1es@naver.com>
2023-12-11 00:47:50 +0900

    C 생성자 추가, A에서 C생성자 테스트

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -3,5 +3,8 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello git! I'm A");
+		System.out.println("Hello, I'm C");
+		C b = new C();
+		b.C_say("test");
 	}
 }
--- MyGit/src/mygit/C.java
+++ MyGit/src/mygit/C.java
@@ -1,7 +1,7 @@
 package mygit;
 
 public class C {
-	public void C_say() {
-		
+	public void C_say(String str) {
+		System.out.println("Class C : " + str);
 	}
 }
